5ce151be7cff0401d955bff936f839e871731b53
Rohit Kumar <r.k2962002@gmail.com>
2022-03-26 08:25:52 +0530

    Added Query Status

--- js/profile.js
+++ js/profile.js
@@ -16,10 +16,15 @@ function hideall() {
     document.getElementById('dashboard-section').classList.add("d-none");
     document.getElementById('profile-section').classList.add("d-none");
     document.getElementById('help-section').classList.add("d-none");
+    document.getElementById('query-section').classList.add("d-none");
 }
 function showdashboard_section() {
     hideall();
     document.getElementById('dashboard-section').classList.remove("d-none");
+}
+function showquery_section() {
+    hideall();
+    document.getElementById('query-section').classList.remove("d-none");
 }
 function showprofile_section() {
     hideall();
@@ -214,5 +219,47 @@ function updatephone() {
             </div>`;
         Alert.innerHTML += AlertText;
     }
+
+}
+
+getQueryDetails();
+
+function getQueryDetails() {
+    firebase.database().ref('Query').on('value', function (snapshot) {
+        document.getElementById("contact-table").innerHTML = "";
+        var e = 1;
+        snapshot.forEach(function (childSnapshot) {
+            data = childSnapshot.val();
+            key = childSnapshot.key;
+            row =
+                `<tr>
+                    <td>${e++}</td>
+                    <td>${data.ID}</td>
+                    <td>${data.Date}</td>
+                    <td>${data.Name}</td>
+                    <td>${data.Add}</td>
+                    <td>${data.Query}</td>
+                    <td>${data.Status}</td>
+                    <td>
+                        <a href="https://web.whatsapp.com/send?phone=+91${data.Cno}" class="table_button" target="_blank"><i class="fa fa-brands fa-whatsapp"></i></a>
+                        <a href="mailto:${data.Email}" class="table_button" target="_blank"><i class="fa fa-envelope"></i></a>
+                    </td>
+                    <td>
+                        <a href="#" class="table_button" onclick="editStudent('${data.ID}')"><i class="fa fa-edit"></i></a>
+                    </td>
+                </tr>`;
+            document.getElementById("contact-table").innerHTML += row;
+        });
+    });
+}
+
+function editStudent(ID) {
+    var Status = prompt("Enter Status");
+    if (Status != null) {
+        firebase.database().ref('Query/' + ID).update({
+            Status: Status
+        });
+    }
+    getQueryDetails();
 
 }

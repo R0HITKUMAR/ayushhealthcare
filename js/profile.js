984c730244705313f36c131a12bee99f38c5cad0
Rohit Kumar <r.k2962002@gmail.com>
2022-03-26 12:15:16 +0530

    Added Hospital Section

--- js/profile.js
+++ js/profile.js
@@ -2,10 +2,17 @@ firebase.auth().onAuthStateChanged((user) => {
     if (!user) {
         location.replace("login.html")
     }
+    // console.log(user);
     var UserEmail = user.email;
     var UserName = user.displayName;
     var UserRoll = user.photoURL;
 
+    document.getElementById('last-login').innerHTML = `
+    <center> Session Infomation</center>
+    Login As: Admin Ayush HealthCare<br>
+    Login through: ${UserEmail}<br>
+    Last Login: ${user.metadata.lastSignInTime}
+    `;
     // Values
     document.getElementById("admin-mail").value = UserEmail;
     document.getElementById("new-name").value = UserName;
@@ -25,13 +32,13 @@ function showdashboard_section() {
     document.getElementById('dashboard-section').classList.remove("d-none");
 }
 
-function showhospital_section(){
+function showhospital_section() {
     hideall();
     document.getElementById('hospital-section').classList.remove("d-none");
 
 }
 
-function showaddhospital_section(){
+function showaddhospital_section() {
     hideall();
     document.getElementById('addhospital-section').classList.remove("d-none");
 }
@@ -48,6 +55,26 @@ function showhelp_section() {
     hideall();
     document.getElementById('help-section').classList.remove("d-none");
 }
+
+// Count Queries
+firebase.database().ref('Query').on('value', function (snapshot) {
+    document.getElementById("total_query").innerHTML = snapshot.numChildren();
+});
+
+
+// Count Resolved Query
+firebase.database().ref('Query').on('value', function (snapshot) {
+    var count = 0;
+    snapshot.forEach(function (childSnapshot) {
+        var childData = childSnapshot.val();
+        if (childData.status == "Resolved") {
+            count++;
+        }
+    });
+    document.getElementById("total_query_resolved").innerHTML = count;
+});
+
+
 
 // Logout Function with Prompt
 function logoutwithprompt() {
@@ -259,7 +286,7 @@ function getQueryDetails() {
                         <a href="mailto:${data.Email}" class="table_button" target="_blank"><i class="fa fa-envelope"></i></a>
                     </td>
                     <td>
-                        <a href="#" class="table_button" onclick="editStudent('${data.ID}')"><i class="fa fa-edit"></i></a>
+                        <a href="#" class="table_button" onclick="editQuery('${data.ID}')"><i class="fa fa-edit"></i></a>
                     </td>
                 </tr>`;
             document.getElementById("contact-table").innerHTML += row;
@@ -267,7 +294,7 @@ function getQueryDetails() {
     });
 }
 
-function editStudent(ID) {
+function editQuery(ID) {
     var Status = prompt("Enter Status");
     if (Status != null) {
         firebase.database().ref('Query/' + ID).update({
@@ -276,4 +303,82 @@ function editStudent(ID) {
     }
     getQueryDetails();
 
+}
+
+funcName();
+async function funcName() {
+    document.getElementById("loader").innerHTML = `
+    <div class="spinner-border text-primary" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>`;
+    const url = "https://ayushservice.azurewebsites.net/getAllInfoToSend/77.512704/28.7473664";
+    const response = await fetch(url);
+    const data = await response.json();
+    D = JSON.stringify(data);
+    getHospitals(D)
+}
+
+
+
+function getHospitals(d) {
+    const Data = JSON.parse(d);
+    var Table = document.getElementById("hospitals-table");
+    Table.innerHTML = "";
+    e = 1;
+    for (var i = 0; i < Data.length; i++) {
+        var d = Data[i];
+        row = `
+        <tr>
+        <td>${e++}</td>
+        <td>${d.hospital_name}</td>
+        <td>${d.address}</td>
+        <td>${d.phone_No}</td>
+        <td>${d.open_time} to ${d.close_time}</td>
+        <td><a href="${d.website_link}" target="_blank" class="table_button" ><i class="fa fa-link"></i></a><td>
+        <td><a onclick="alert('Coming Soon')" class="table_button" ><i class="fa fa-edit"></i></a></td>
+        </tr>
+        `;
+        Table.innerHTML += row;
+    }
+    document.getElementById("loader").innerHTML = "";
+
+}
+
+function datatable(status, ID) {
+    if (status == true) {
+        $(ID).DataTable({
+            "pageLength": 40,
+            dom: 'Bfrtip',
+            paging: false,
+            destroy: true,
+            buttons: [
+                {
+                    extend: 'copyHtml5',
+                    exportOptions: {
+                        columns: [0, 1, 2, 3, 4, 5, 6]
+                    }
+                },
+                {
+                    extend: 'excelHtml5',
+                    exportOptions: {
+                        columns: [0, 1, 2, 3, 4, 5, 6]
+                    }
+                },
+                {
+                    extend: 'print',
+                    exportOptions: {
+                        columns: [0, 1, 2, 3, 4, 5, 6]
+                    }
+                },
+                {
+                    extend: 'pdfHtml5',
+                    exportOptions: {
+                        columns: [0, 1, 2, 3, 4, 5, 6]
+                    }
+                }
+            ]
+        });
+    } else {
+        $(ID).DataTable().destroy();
+    }
 }
